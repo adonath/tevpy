d90c518928df0c798556dd826a9e0749d270acce
Axel Donath <axel.donath@mpi-hd.mpg.de>
2019-08-28 10:35:18 +0200

    Update

--- gammapy/cube/estimator.py
+++ gammapy/cube/estimator.py
@@ -21,66 +21,96 @@ class MapDatasetEstimator:
         Map geometry used for counts and background map. Defines the
         energy axes in reconstructed energy and typically has fine
         spatial resolution.
-    geom_irf : `~gammapy.maps.MapGeom`
-        Map geometry used for the exposure, energy and psf map, defines
-        the energy axis in true energy and typically has a coarse
-        spatial resolution.
+    name : str
+        Dataset name.
+    energy_axis_true: `~MapAxis`
+        True energy axis.
+    rad_axis : `MapAxis`
+        Rad axis for PSF map.
+    migra_axis : `MapAxis`
+        Migra axis for EDISP map.
+    binsz_irf : str or `Quantity`
+        Spatial bin size for the PSF and EDISP map.
+    background_oversampling : int
+        Background oversampling factor in energy.
     offset_max : `~astropy.units.Quantity`
         Maximum offset cut.
     safe_energy_range: `~astropy.units.Quantity`
         Safe energy range.
+    cutout_observation : bool
+        Make a cutout of the geometry for the observation.
     """
     default_units = {
-        "exposure" : "m2 s",
+        "exposure": "m2 s",
         "psf": "sr-1"
     }
 
-    def __init__(self, observation, geom, geom_irf=None, offset_max=None, safe_energy_range=None):
-        self.observation = observation
+    def __init__(
+            self,
+            geom,
+            name="map-dataset",
+            energy_axis_true=None,
+            rad_axis=None,
+            migra_axis=None,
+            binsz_irf="0.2 deg",
+            background_oversampling=None,
+            offset_max="3 deg",
+            safe_energy_range="aeff 10%",
+            cutout_observation=True
+    ):
         self.geom = geom
-        self.geom_irf = geom_irf
+        self.name = name
+        self.energy_axis_true = energy_axis_true
+        self.rad_axis = rad_axis
+        self.migra_axis = migra_axis
+        self.binsz_irf = Angle(binsz_irf)
+        self.background_oversampling = background_oversampling
         self.offset_max = Angle(offset_max)
         self.safe_energy_range = safe_energy_range
-
-    @property
-    def ontime(self):
-        """On time"""
-        return self.observation.observation_live_time_duration
-
-    def _offset(self, geom):
-        geom_image = geom.to_image()
-        coords = geom_image.get_coord()
-        pointing = self.observation.pointing_radec
-        offset = pointing.separation(coords.skycoord)
-        return offset
+        self.cutout_observation = cutout_observation
 
     @lazyproperty
-    def offset(self):
-        return self._offset(self.geom)
+    def geom_image(self):
+        return self.geom.to_image()
 
     @lazyproperty
-    def offset_irf(self):
-        """Offset array"""
-        return self._offset(self.geom_irf)
+    def geom_image_irf(self):
+        return self.geom.to_image()
 
     @lazyproperty
-    def skycoord(self):
-        coord = self.geom.to_image().get_coord()
+    def _skycoord(self):
+        coord = self.geom_image.get_coord()
         return coord.skycoord
 
     @lazyproperty
-    def fov_coords(self):
-        pointing = self.observation.pointing_radec
+    def _skycoord_irf(self):
+        coord = self.geom_image_irf.get_coord()
+        return coord.skycoord
+
+    @lru_cache(maxsize=1)
+    def offset(self, observation):
+        return self.geom.separation(observation.pointing_radec)
+
+    @lru_cache(maxsize=1)
+    def offset_irf(self, observation):
+        """Offset array"""
+        slices = self.geom_irf.cutout_slices(position=observation.pointing_radec, width=self.offset_max)
+        skycoord = self.skycoord_irf[slices]
+        return skycoord.separation(observation.pointing_radec)
+
+    @lru_cache(maxsize=1)
+    def fov_coords(self, observation):
+        pointing = observation.pointing_radec
         frame = SkyOffsetFrame(origin=pointing)
         pseudo_fov_coord = self.skycoord.transform_to(frame)
         fov_lon = pseudo_fov_coord.lon
         fov_lat = pseudo_fov_coord.lat
         return fov_lon, fov_lat
 
-    @lazyproperty
-    def fov_coords_alt_az(self):
+    @lru_cache(maxsize=1)
+    def fov_coords_alt_az(self, observation):
         """FoV coordinates"""
-        pointing = self.observation.fixed_pointing_info
+        pointing = observation.fixed_pointing_info
         coord_altaz = self.skycoord.transform_to(pointing.altaz_frame)
 
         # Compute FOV coordinates of map relative to pointing
@@ -88,26 +118,26 @@ class MapDatasetEstimator:
                 coord_altaz.az, coord_altaz.alt, pointing.altaz.az, pointing.altaz.alt
             )
 
-    def estimate_counts(self):
-        """Estimate counts map.
+    def prepare_counts(self, observation):
+        """Prepare counts map.
 
         Parameters
         ----------
-        events : `EventList`
-            List of events.
-        geom :
+        observation : `DataStoreObservation`
+            Observation to compute counts map for.
 
         Returns
         -------
         counts : `Map`
             Counts map.
         """
-        counts = Map.from_geom(self.geom)
-        coord = self.observation.events._get_coord_from_geom(self.geom)
+        geom = self._cutout_geom(self.geom, observation)
+        counts = Map.from_geom(geom)
+        coord = observation.events._get_coord_from_geom(geom)
         counts.fill_by_coord(coord)
         return counts
 
-    def estimate_background(self):
+    def prepare_background(self, observation):
         """Estimate background map.
 
         Returns
@@ -118,31 +148,32 @@ class MapDatasetEstimator:
         # Get altaz coords for map
         energies = self.geom.get_axis_by_name("energy").edges
 
-        bkg_coordsys = self.observation.bkg.meta.get("FOVALIGN", "ALTAZ")
+        bkg_coordsys = observation.bkg.meta.get("FOVALIGN", "ALTAZ")
 
         if bkg_coordsys == "ALTAZ":
-            fov_lon, fov_lat = self.fov_coords_alt_az
+            fov_lon, fov_lat = self.fov_coords_alt_az(observation)
         elif bkg_coordsys == "RADEC":
-            fov_lon, fov_lat = self.fov_coords
+            fov_lon, fov_lat = self.fov_coords(observation)
         else:
             raise ValueError(
                 'Found unknown background coordinate system definition: "{}". '
                 'Should be "ALTAZ" or "RADEC".'.format(bkg_coordsys)
             )
 
-        bkg_de = self.observation.bkg.evaluate_integrate(
+        bkg_de = observation.bkg.evaluate_integrate(
             fov_lon=fov_lon,
             fov_lat=fov_lat,
             energy_reco=energies[:, np.newaxis, np.newaxis],
         )
 
         d_omega = self.geom.solid_angle()
-        data = (bkg_de * d_omega * self.ontime).to_value("")
+        ontime = observation.observation_live_time_duration
+        data = (bkg_de * d_omega * ontime).to_value("")
 
         background = Map.from_geom(self.geom, data=data)
         return background
 
-    def estimate_exposure(self):
+    def prepare_exposure(self, observation):
         """Estimate exposure map.
 
         Returns
@@ -150,8 +181,10 @@ class MapDatasetEstimator:
         exposure : `Map`
             Exposure map.
         """
-        energy = self.geom_irf.get_axis_by_name("energy").center
-        exposure = self.observation.aeff.data.evaluate(
+        geom = self.geom_exposure
+
+        energy = geom.get_axis_by_name("energy").center
+        exposure = observation.aeff.data.evaluate(
             offset=self.offset_irf, energy=energy[:, np.newaxis, np.newaxis]
         )
 
@@ -236,7 +269,7 @@ class MapDatasetEstimator:
         energy_mask = self.geom.energy_mask(emin=emin, emax=emax)
         return offset_mask & energy_mask
 
-    def run(self, steps="all"):
+    def run(self, observation, steps="all"):
         """"""
         kwargs = {}
 
@@ -244,10 +277,10 @@ class MapDatasetEstimator:
             steps = ["counts", "exposure", "background", "psf", "edisp"]
 
         if "counts" in steps:
-            kwargs["counts"] = self.estimate_counts()
+            kwargs["counts"] = self.prepare_counts(observation)
 
         if "exposure" in steps:
-            kwargs["exposure"] = self.estimate_exposure()
+            kwargs["exposure"] = self.prepare_exposure(observation)
 
         if "background" in steps:
             background = self.estimate_background()
@@ -266,23 +299,87 @@ class MapDatasetEstimator:
             # TODO: set reference to exposure map?
 
         kwargs["mask_safe"] = self.estimate_mask_safe()
-
+        kwargs["gti"] = observation.gti
         return MapDataset(**kwargs)
 
 
-def _run_map_estimator(obs, offset_max, geom, geom_irf, steps, path=None, overwrite=True):
-    """"""
-    position = obs.pointing_radec
-    width = 2 * offset_max
-    geom = geom.cutout(position=position, width=width)
-    geom_irf = geom_irf.cutout(position=position, width=width)
-    estimator = MapDatasetEstimator(observation=obs, geom=geom, geom_irf=geom_irf, offset_max=offset_max)
-    dataset = estimator.run(steps=steps)
+class EstimatorChain:
+    """Estimator chain.
 
-    if path is not None:
-        filename = Path(path) / "obs_{}.fits".format(obs.obs_id)
-        dataset.write(filename, overwrite=overwrite)
-    else:
+    Parameters
+    ----------
+    estimator : list of `Estimator`
+        List of estimators to execute in a chain.
+
+    """
+
+    def __init__(self, estimators):
+        self.estimators = estimators
+
+    def run(self, observation):
+        """Run estimator chain.
+
+        """
+
+        dataset = self.estimators[0].run(observation)
+
+        for estimator in self.estimators[1:]:
+            dataset = estimator.run(dataset)
+
+        return dataset
+
+
+class MapDatasetToSpectrumDatasetEstimator:
+    """Reduce a map dataset to a spectrum dataset.
+
+    Parameters
+    ----------
+    region : `SkyRegion`
+        REgion to integrate over.
+    apply_containment_correction : bool
+        Whether to apply the containment correction of the PSF.
+
+    """
+    def __init__(self, region, apply_containment_correction=True):
+        self.region = region
+        self.apply_containment_correction = apply_containment_correction
+
+    def run(self, dataset):
+        """"""
+
+        spectrum_dataset = dataset
+
+        return spectrum_dataset
+
+
+class MapDatasetToImageEstimator:
+    """Prepare images.
+
+    Parameters
+    ----------
+    spectrum : `SpectralModel`
+        Spectral model used for weighting.
+    keepdims : bool
+        Keep extra dimensions.
+
+    """
+    def __init__(self, spectrum, keepdims=True):
+        self.spectrum = spectrum
+        self.keepdims = keepdims
+
+    def run(self, dataset):
+        """Run image preparator.
+
+        Parameters
+        ----------
+        dataset : `MapDataset`
+            Input map dataset.
+
+        Returns
+        -------
+        dataset : `MapDataset`
+            Output map dataset.
+        """
         return dataset
 
 
@@ -300,12 +397,8 @@ class DatasetsEstimator:
         Map geometry used for exposure, edisp and PSF map.
     """
 
-    def __init__(self, geom, geom_irf, offset_max=None, background_estimator=None,
-                 time_intervals=None):
-        self.geom = geom
-        self.geom_irf = geom_irf
-        self.offset_max = Angle(offset_max)
-        self.background_estimator = background_estimator
+    def __init__(self, preparators, time_intervals=None):
+        self.preparators = preparators
         self.time_intervals = time_intervals
 
     def stack(self, datasets):
@@ -361,7 +454,6 @@ class DatasetsEstimator:
         estimator = MapDatasetEstimator(observation=obs, geom=geom, geom_irf=geom_irf, offset_max=self.offset_max)
         return estimator.run(steps=steps)
 
-
     def run(self, observations, steps="all", n_jobs=4):
         """Run datasets estimator.
 
@@ -388,8 +480,6 @@ class DatasetsEstimator:
                 datasets.append(dataset)
 
         return datasets
-
-
 
     def run_write(self, observations, steps="all", path="datasets", overwrite=False):
         """Run datasets estimator and write datasets to disk.
@@ -419,15 +509,17 @@ class DatasetsEstimator:
                 dataset.write(filename, overwrite=overwrite)
 
 
-# needed?
 class BackgroundNormEstimator:
-    ""
+    """
 
-    def __init__(self, exclusion_mask=None, method="fit"):
+    """
+
+    def __init__(self, exclusion_mask=None, method="fit", source_model=None):
         self.exclusion_mask = exclusion_mask
+        self.method = method
+        self.source_model = source_model
 
-    @staticmethod
-    def fit(dataset):
+    def fit(self, dataset):
         # fit background norm
         dataset.parameters.freeze_all()
         dataset.background_model.norm.frozen = False
@@ -436,14 +528,15 @@ class BackgroundNormEstimator:
         result = fit.optimize()
         return dataset
 
-    @staticmethod
-    def scale(dataset):
+    def scale(self, dataset):
         # scale background outside exclusion regions
         return dataset
 
     def run(self, dataset):
         """Run estimator
         """
-        datsaet = self.fit(dataset)
-        dataset = self.scale(dataset)
+        if self.method == "fit":
+            dataset = self.fit(dataset)
+        elif self.method == "scale":
+            dataset = self.scale(dataset)
         return dataset
